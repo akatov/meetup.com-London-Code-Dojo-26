1aa9e1bb21aed046c65761bb0f528508031076dd
Dmitri Akatov <akatov@gmail.com>
2013-09-09 19:33:25 +0100

    create-test

--- test/workflow/core_test.clj
+++ test/workflow/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [workflow.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest create-test
+  (testing "we can create a state"
+    (is (= obj :new-permit))))
