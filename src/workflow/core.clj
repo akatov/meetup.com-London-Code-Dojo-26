db12de6a0ea04f2223385dcd01d1f50116e4f9b9
Dmitri Akatov <akatov@gmail.com>
2013-09-09 21:05:36 +0100

    fix the test

--- src/workflow/core.clj
+++ src/workflow/core.clj
@@ -7,7 +7,9 @@
       (= name :create) :new-permit)))
 
 (defn transition
-  [current-state]
-  (cond
-   (= current-state :new-permit) :submitted-permit
-   (= current-state :submitted-permit) :approved-permit))
+  ([current-state]
+     (cond
+      (= current-state :new-permit) :submitted-permit
+      (= current-state :submitted-permit) :approved-permit))
+  ([current-state transition-name]
+     :rejected-permit))
