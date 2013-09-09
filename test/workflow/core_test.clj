a03f11da5d4e469bc24c29a68e5579f8f9c71cab
Dmitri Akatov <akatov@gmail.com>
2013-09-09 20:10:37 +0100

    pass current state in other test

--- test/workflow/core_test.clj
+++ test/workflow/core_test.clj
@@ -9,7 +9,7 @@
 
 (deftest test-that-new-permit-can-change-to-submitted-permit-through-submit-action
   (testing "Test that new permit can change to submitted permit through submit action"
-    (let [new-state (transition)]
+    (let [new-state (transition :new-permit)]
       (is (= new-state :submitted-permit)))))
 
 (deftest test-that-submitted-permit-can-change-to-approved-permit-through-approve-action
